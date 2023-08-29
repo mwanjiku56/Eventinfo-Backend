ca42a115e05e5e853bd3e0bec91ec940d5be1f84
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2023-08-29 11:08:43 +0300

    added the routes for each method in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
   resources :bookings
-  resources :events
-  resources :users
+  resources :events, only [:show, :index]
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
